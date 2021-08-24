d60cddd66ab38d24cccb5838508135410a4cc00b
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-08-24 21:07:32 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -78,7 +78,7 @@ async def start(bot, update):
     
     await bot.send_photo(
         chat_id=update.chat.id,
-        captionhttps://telegra.ph/file/c59b4fe876ddce348a7ae.jpg",
+        caption="https://telegra.ph/file/c59b4fe876ddce348a7ae.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
